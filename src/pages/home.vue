f5937b9dcadee50a25fbbdd08744d91cdb4af064
transformGit <2630243397@qq.com>
2017-11-15 18:54:00 +0800

    add new home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
 	<section>
 		<div class="banner" v-loading="loading">
-			<el-carousel indicator-position height="500px">
+			<el-carousel indicator-position height="600px">
 		    <el-carousel-item v-for="(item, index) in bannerList" :key="index">
 		      <router-link :to='item.url+"&&wareName="+item.wareName+"&wareImg="+item.imgUrl' :title="item.wareName" target="_blank">
 		      	<img :src="item.imgUrl" :alt="item.wareName">
@@ -9,168 +9,45 @@
 		    </el-carousel-item>
 		  </el-carousel>
 		</div>
-		<div class="main-wrap">
-			<section> 
-				<div class="title">
-					<h3><em>热门推荐</em></h3>
-					<h3>hot recommendation</h3>
-					<router-link to="list?title=热门推荐" class="transition" target="_blank">
-						show more
-						<i class="el-icon-d-arrow-right"></i>
-					</router-link>
+		<div class="main">
+			<div class="container">
+				<div class="section1">
+					<div class="title">
+						<h2>全球100+旅拍目的地</h2>
+						<h4 class="uppercase">GLOBAL TRAVEL DESTINATIONS</h4>
+						<div class="line"></div>
+					</div>
+					<div class="destinations">
+						<ul class="des-row">
+							<li v-for="des in desRow1" :index="des.id">
+								<router-link :to="des.href">{{des.name}}</router-link>
+							</li>
+						</ul>
+						<ul class="des-row">
+							<li v-for="des in desRow2" :index="des.id">
+								<router-link :to="des.href">{{des.name}}</router-link>
+							</li>
+						</ul>
+						<ul class="des-row">
+							<li v-for="des in desRow3" :index="des.id">
+								<router-link :to="des.href">{{des.name}}</router-link>
+							</li>
+						</ul>
+					</div>
+					<div class="product">
+						<div class="recommend-row">	
+							<div class="more">
+								<router-link to="/">更多推荐>></router-link>
+							</div>
+							<ul class="recommend-list">
+								<li v-for="item in recommendList">
+									<img :src="item.imgUrl" alt="">
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
-				<div class="hot image-list">
-					<el-row :gutter="15">
-						<el-col :span="9">
-							<router-link to="list?title=热门推荐" target="_blank">
-								<img :src="hotImgList[0].imgUrl" :alt="hotImgList[0].wareName"/>
-								<div class="text transition">
-									<span>{{hotImgList[0].wareName}}</span>
-								</div>
-							</router-link>
-						</el-col>
-						<el-col :span="8">
-							<el-row v-for="(item, index) in hotSmallImgList" :key="index">
-								<router-link :to="item.url" target="_blank">
-									<img :src="item.imgUrl" :alt="item.wareName"/>
-									<div class="text transition">
-										<span>{{item.wareName}}</span>
-									</div>
-								</router-link>
-							</el-row>
-						</el-col>
-						<el-col :span="7">
-							<router-link to="list?title=热门推荐" target="_blank">
-								<img :src="hotImgList[1].imgUrl" :alt="hotImgList[1].wareName"/>
-								<div class="text transition"><span>{{hotImgList[1].wareName}}</span></div>
-							</router-link>
-						</el-col>
-					</el-row>
-				</div>
-			</section>
-			<section>
-				<div class="title">
-					<h3><em>国内旅拍</em></h3>
-					<h3>native travel</h3>
-					<router-link to="list?title=国内旅拍" class="transition" target="_blank">
-						show more
-						<i class="el-icon-d-arrow-right"></i>
-					</router-link>
-				</div>
-				<div class="nationwide image-list">
-					<el-row :gutter="15">
-						<el-col :span="12" v-for="(item, index) in nationwideLargeImgList" :key="index">
-							<router-link to="list?title=国内旅拍" target="_blank">
-								<img v-lazyload="item.imgUrl" :alt="item.wareName"/>
-								<div class="text transition">
-									<span>{{item.wareName}}</span>
-								</div>
-							</router-link>
-						</el-col>
-					</el-row>
-					<el-row :gutter="15">
-						<el-col :span="6" v-for="(item, index) in nationwideImgList" :key="index">
-							<router-link :to="item.url+'&&wareName='+item.wareName" :title="item.wareName" target="_blank">
-								<img v-lazyload="item.imgUrl" :alt="item.wareName"/>
-								<div class="text transition"><span>{{item.wareName}}</span></div>
-							</router-link>
-						</el-col>
-					</el-row>
-				</div>
-			</section>
-			<section>
-				<div class="title">
-					<h3><em>海外摄影</em></h3>
-					<h3>overseas photography</h3>
-					<router-link to="list?title=海外摄影" class="transition" target="_blank">
-						show more
-						<i class="el-icon-d-arrow-right"></i>
-					</router-link>
-				</div>
-				<div class="overseas image-list">
-					<el-row :gutter="15">
-						<el-col :span="12">
-							<router-link to="list?title=海外摄影" target="_blank">
-								<img :src="overseasList[0].imgUrl" :alt="overseasList[0].wareName"/>
-								<div class="text transition">
-									<span>{{overseasList[0].wareName}}</span>
-								</div>
-							</router-link>
-						</el-col>
-						<el-col :span="12">
-							<el-row :gutter="15">
-								<el-col :span="12" v-for="(item, index) in overseasList" :key="index">
-									<router-link :to="item.url" target="_blank">
-										<img :src="item.imgUrl" :alt="item.wareName"/>
-										<div class="text transition">
-											<span>{{item.wareName}}</span>
-										</div>
-									</router-link>
-								</el-col>
-							</el-row>
-						</el-col>
-					</el-row>
-					<el-row :gutter="15">
-						<el-col :span="6">
-							<el-row :gutter="15">
-								<el-col :span="24" v-for="(item, index) in hotImgList" :key="index">
-									<router-link :to="item.url" target="_blank">
-										<img :src="item.imgUrl" :alt="item.wareName"/>
-										<div class="text transition">
-											<span>{{item.wareName}}</span>
-										</div>
-									</router-link>
-								</el-col>
-							</el-row>
-						</el-col>
-						<el-col :span="12">
-							<router-link to="list?title=海外摄影" target="_blank">
-								<img :src="hotImgList[0].imgUrl" :alt="hotImgList[0].wareName"/>
-								<div class="text transition">
-									<span>{{hotImgList[0].wareName}}</span>
-								</div>
-							</router-link>
-						</el-col>
-						<el-col :span="6">
-							<el-row :gutter="15">
-								<el-col :span="24" v-for="(item, index) in hotImgList" :key="index">
-									<router-link :to="item.url" target="_blank">
-										<img :src="item.imgUrl" :alt="item.wareName"/>
-										<div class="text transition">
-											<span>{{item.wareName}}</span>
-										</div>
-									</router-link>
-								</el-col>
-							</el-row>
-						</el-col>
-					</el-row>
-				</div>
-			</section>
-			<section>
-				<div class="title">
-					<h3><em>景点/客片展示</em></h3>
-					<h3>samples show</h3>
-					<router-link to="list?title=景点/客片展示" class="transition" target="_blank">
-						show more
-						<i class="el-icon-d-arrow-right"></i>
-					</router-link>
-				</div>
-				<div class="samples image-list">
-					<el-row :gutter="15">
-						<el-col :span="6" v-for="(item, index) in samplesImgList" :key="index">
-							<router-link :to="item.url+'&wareName='+item.wareName" target="_blank">
-								<img v-lazyload="item.imgUrl" :alt="item.wareName"/>
-								<div class="text transition">
-									<span>{{item.wareName}}</span>
-								</div>
-							</router-link>
-						</el-col>
-					</el-row>
-				</div>
-			</section>
-			<section>
-				<img src="http://www.fookvision.com//Public/Wwwfookvisioncom/images/index/advent.jpg">
-			</section>
+			</div>
 		</div>
 	</section>
 </template>
@@ -181,6 +58,43 @@
 			return {
 				loading: false,
 				bannerList: [],
+				destinations: [
+					{ id: 1, name: '丽江', href: '/', row: 1 },
+					{ id: 2, name: '大理', href: '/', row: 1 },
+					{ id: 3, name: '普吉岛', href: '/', row: 1 },
+					{ id: 4, name: '巴厘岛', href: '/', row: 1 },
+					{ id: 5, name: '马尔代夫', href: '/', row: 1 },
+					{ id: 6, name: '巴黎', href: '/', row: 1 },
+					{ id: 7, name: '圣托里尼', href: '/', row: 1 },
+					{ id: 8, name: '罗马', href: '/', row: 1 },
+					{ id: 9, name: '厦门', href: '/', row: 1 },
+					{ id: 10, name: '北京', href: '/', row: 1 },
+					{ id: 11, name: '上海', href: '/', row: 1 },
+					
+					{ id: 12, name: '高雄', href: '/', row: 2 },
+					{ id: 13, name: '垦丁', href: '/', row: 2 },
+					{ id: 14, name: '苏梅岛', href: '/', row: 2 },
+					{ id: 15, name: '济州岛', href: '/', row: 2 },
+					{ id: 16, name: '塞班岛', href: '/', row: 2 },
+					{ id: 17, name: '新西兰', href: '/', row: 2 },
+					{ id: 18, name: '墨尔本', href: '/', row: 2 },
+					{ id: 19, name: '东京', href: '/', row: 2 },
+					{ id: 20, name: '马来西亚', href: '/', row: 2 },
+					{ id: 21, name: '意大利', href: '/', row: 2 },
+					
+					{ id: 22, name: '三亚', href: '/', row: 3 },
+					{ id: 23, name: '大连', href: '/', row: 3 },
+					{ id: 24, name: '青岛', href: '/', row: 3 },
+					{ id: 25, name: '深圳', href: '/', row: 3 },
+					{ id: 26, name: '布拉格', href: '/', row: 3 },
+					{ id: 27, name: '西班牙', href: '/', row: 3 },
+					{ id: 28, name: '维也纳', href: '/', row: 3 },
+				],
+				recommendList: [
+					{ id: '20171115001', name: '日本东京旅拍婚纱照一天套系', imgUrl: 'http://fileServer.yueshijue.com/fileService/uploads/2017/11/01/415095265233560.jpg',},
+					{ id: '20171115002', name: '日本东京旅拍婚纱照一天套系', imgUrl: 'http://fileServer.yueshijue.com/fileService/uploads/2017/11/01/415095265233560.jpg',},
+					{ id: '20171115003', name: '日本东京旅拍婚纱照一天套系', imgUrl: 'http://fileServer.yueshijue.com/fileService/uploads/2017/11/01/415095265233560.jpg',},
+				],
 				hotImgList: [{
 					url: '',
 					imgUrl: '',
@@ -233,6 +147,17 @@
 				})
 			},
 		},
+		computed: {
+			desRow1() {
+				return this.destinations.filter(des => des.row === 1)
+			},
+			desRow2() {
+				return this.destinations.filter(des => des.row === 2)
+			},
+			desRow3() {
+				return this.destinations.filter(des => des.row === 3)
+			},
+		},
 		created() {
 			this.getBannerList()
 			this.getImgList()
@@ -244,150 +169,99 @@
 		width: 100%;
 		overflow: hidden;
 	}
-  .el-carousel__item {
-  	a {
-  		display: inline-block;
-  		width: 100%;
-  		height: 100%;
-  	}
-  }
-  .main-wrap {
-  	width: 1180px;
-  	margin: 20px auto;
-  	section {
-  		margin: 60px 0;
-  	}
-  	.title {
-  		position: relative;
-  		margin: 30px 0;
-  		text-align: center;
-  		em {
-  			font-style: normal;
-  			font-weight: bold;
-  			&::before, &::after {
-  				content: '';
-					position: absolute;
-					top: 10px;
-					width: 300px;
-  				height: 1px;
-  				background: #ddd
-  			}
-  			&::before {
-  				margin-left: -320px;
-  			}
-  			&::after {
-					margin-left: 20px;
-  			}
-  		}
-  		h3 {
-  			margin: 20px 0;
-  			font-weight: bold;
-  		}
-  		h3:nth-child(2) {
-  			color: #0095d9;
-  			text-transform: uppercase;
-  		} 
-  		a {
-  			position: absolute;
-  			right: 10px;
-  			bottom: -20px;
-  			font-size: 13px;
-  			color: #0094DA;
-  			font-weight: bold;
-  			text-transform: uppercase;
-  			&:hover {
-  				right: 0;
-  				color: #0095d9;
-  			}
-  			i {
-  				font-size: 12px
-  			}
-  		}
-  	}
-  	.image-list {
-  		a {
-  			position: relative;
-  			display: block;
-  			width: 100%;
-  			height: 100%;
-  			&:hover {
-  				.text{
-  					display: block;
-  				}
-  			}
-  			.text {
-  				display: none;
-  				position: absolute;
-  				top: 0;
-  				width: 100%;
-  				height: 100%;
-  				text-align: center;
-  				color: #fff;
-  				background: rgba(0,0,0,.3);
-  				span {
-  					position: absolute;
-  					bottom: 0;
-  					left: 0;
-  					right: 0;
-  					padding: 8px 0;
-  					background: rgba(0,0,0,.3)
-  				}
-  			}
-  		}
-  	}
-  	img {
-  		width: 100%;
-  		height: 100%
-  	}
-  }
-  .el-row {
-  	margin-bottom: 20px;
-  	.el-col {
-  		height: 100%;
-  	}
+  // .el-carousel__item {
+  // 	a {
+  // 		display: inline-block;
+  // 		width: 100%;
+  // 		height: 100%;
+  // 	}
+  // }
+  .title {
+		padding: 30px;
+		h2 {
+			font-size: 32px;
+			letter-spacing: 2px;
+		}
+		h4 {
+			margin: 15px 0;
+			font-size: 12px;
+		}
+		.line {
+			width: 50px;
+			height: 2px;
+			margin: auto;
+			background: #c60c1a;
+		}
 	}
-  .hot {
-  	.el-col {
-			height: 275px
-  	}
-  	.el-col:nth-child(2) {
-  		.el-row {
-  			height: 130px;
-  			&:first-child {
-  				margin-bottom: 15px;
-  			}
-  		}
-  	}
-  }
-  .nationwide {
-  	.el-row {
-  		margin-bottom: 15px
-  	}
-  	.el-col {
-  		height: 260px
-  	}
-  }
-  .overseas {
-  	.el-row {
-  		margin-bottom: 15px;
-  	}
-  	.el-col {
-			height: 355px;
-			.el-row {
-				.el-col {
-					height: 170px;
-					margin-bottom: 15px
+	.destinations {
+		width: 100%;
+		.des-row {
+			display: flex;
+			justify-content: center;
+			margin-bottom: 15px;
+			a {
+				display: block;
+				width: 105px;
+				height: 30px;
+				line-height: 30px;
+				margin-left: 9px;
+				color: #666;
+				background: #ccc;
+				border-radius: 5px;
+				position: relative;
+				&::before {
+					content: '';
+					position: absolute;
+					top: 9px;
+					left: 9px;
+					width: 10px;
+					height: 12px;
+					background: url(../assets/img/coordinate.png) no-repeat center;
+				}
+				&:hover {
+					color: #fff;
+					background: #e50110;
+					&::before {
+						background: url(../assets/img/coordinate_hover.png) no-repeat center;
+					}
 				}
 			}
-  	}
-  }
-  .samples {
-  	.el-row {
-  		margin-bottom: -20px;
-  		.el-col{
-				height: 200px;
-				margin-bottom: 15px;
-  		}
+		}
+	}
+	.product {
+		margin: 50px 0;
+		.recommend-row {
+			.more {
+				margin: 12px 0;
+				text-align: right;
+				a {
+					color: #e50110;
+				}
+			}
+		}
+		.recommend-list {
+			display: flex;
+			li {
+				flex: 1;
+				margin-right: 9px;
+				&:last-child {
+					margin-right: 0;
+				}
+				img {
+					display: block;
+					width: 100%;
+					height: auto;
+				}
+			}
+		}
+	}
+  .main {
+  	color: #fff;
+  	background: #212121;
+  	.section1 {
+  		padding: 15px 0;
+  		text-align: center;
+  		
   	}
   }
 </style>
